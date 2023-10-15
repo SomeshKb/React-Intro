4cecc82921f75e8f832d23c87a89d58f20d8e344
SomeshKb <someshkumarbhaskar@gmail.com>
2023-10-16 03:01:05 +0530

    2 files modified

--- src/component/post.tsx
+++ src/component/post.tsx
@@ -40,9 +40,11 @@ export default function Post() {
   };
 
   return (
-    <div className="post-container">
-      <div className="post-title">{post?.title}</div>
-      <div className="post-body">{post?.body}</div>
+    <div className="post-details">
+      <div className="post-container">
+        <div className="post-title">{post?.title}</div>
+        <div className="post-body">{post?.body}</div>
+      </div>
 
       <div className="comment-wrapper">
         <span className="comment-header">Comments</span>
